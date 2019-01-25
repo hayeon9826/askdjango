98f4ae7eb0ab2ecf2f5c244671cf00415692ada0
Hayeon <hayeon226@likelion.org>
2019-01-25 14:28:18 +0900

    dojo. hello 함수

--- dojo/views.py
+++ dojo/views.py
@@ -7,3 +7,6 @@ def mysum(request, numbers):
     #numbers = '1/2/12/123/1232/1232/3213/1232'
     result = sum(map(int, numbers.split('/')))
     return HttpResponse(result)
+
+def hello(request, name, age):
+    return HttpResponse('안녕하세요. {}. {}살이시네요.'.format(name, age))
