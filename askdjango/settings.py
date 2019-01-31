285a1111a1536790e767fe64c3deb27825aa5785
Hayeon <hayeon226@likelion.org>
2019-02-01 03:03:11 +0900

    blog.models.Post 모델 생성 / migration / admin

--- askdjango/settings.py
+++ askdjango/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'dojo',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -110,13 +111,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Author(models.Model):
@@ -12,10 +13,17 @@ class Author(models.Model):
 
 class Post(models.Model):
     writer = models.ForeignKey(Author, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100,verbose_name='제목', help_text="포스팅 제목을 입력해주세요. 최대 100자 내외") #verbose_name: 어드민 한글로 설정
+    category = models.CharField(max_length=10, default = True,
+        choices = (
+            ('제목1', '세간'), #저장될 값 /  UI에 보여질 레이블
+            ('제목2', '픽즐'),
+            ('제목3', '모잇')
+        )) # 길이 제한이 있는 문자열
     content = models.CharField(max_length=500)
-    date = models.DateField()
-    likes = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    #likes = models.IntegerField()
 
     def __str__(self):
         return self.title
