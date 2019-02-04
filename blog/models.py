462fb89052e56ef83dc7c1423552675e53e91734
Hayeon <hayeon226@likelion.org>
2019-02-05 00:43:47 +0900

    model field 공부

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 from django.conf import settings
+import re
+from django.forms import ValidationError
+from django.utils import timezone
+
+
+def lnglat_validator(value):
+    if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)$', value): #[+-]?는 없거나 있거나
+        raise ValidationError("Invalid LngLat Type")
 
 # Create your models here.
 class Author(models.Model):
@@ -20,9 +28,14 @@ class Post(models.Model):
             ('제목2', '픽즐'),
             ('제목3', '모잇')
         )) # 길이 제한이 있는 문자열
-    content = models.CharField(max_length=500)
+    content = models.TextField(help_text='Markdown 문법을 써주세요.')
+    tags = models.CharField(max_length = 100, blank = True) #옵션필드 (blank = true)
+    lnglat = models.CharField(max_length=50, blank = True, #옵션필드 (blank = True)
+        validators = [lnglat_validator],
+        help_text = '경도, 위도 포맷으로 입력')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+    test_field = models.IntegerField(default=10)
     #likes = models.IntegerField()
 
     def __str__(self):
