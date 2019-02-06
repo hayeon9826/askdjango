370e62eb90f43be2cf85887a4973bd7c761897be
Hayeon <hayeon226@likelion.org>
2019-02-06 23:15:06 +0900

    admin 기능 공부

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from .models import Post, Author
+from django.utils.safestring import mark_safe
 
 # Register your models here.
 admin.site.register(Post)
+admin.site.unregister(Post)
 admin.site.register(Author)
+admin.site.unregister(Author)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'content', 'content_size']
+
+    def content_size(self, post):
+        return mark_safe('<strong>{}</strong>글자'.format(len(post.content)))
+    content_size.short_description = '내용 글자수'
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'job']
